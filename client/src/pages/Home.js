b9a74beb9ab8e633e847e564b76d159f6c815815
wmt36 <wmt36@Williams-iMac.local>
2020-10-31 12:06:58 -0600

    updated component list with bootstrap link added as well as starter code for the home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react'
+import Container from '../components/Container';
+import Col from '../components/Col';
+import Row from '../components/Row';
+import Btn from '../components/Btn';
+import ResultsCard from '../components/ResultsCard';
