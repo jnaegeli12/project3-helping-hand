84f94539170223689ab1cdc86282e1e5781d9b2b
wmt36 <wmt36@Williams-iMac.local>
2020-11-07 12:37:46 -0700

    updated api routes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
+const dbModel = require("./db");
 
 // Define middleware here
 router.use(express.urlencoded({ extended: true }));
@@ -12,7 +13,10 @@ if (process.env.NODE_ENV === "production") {
   router.use(express.static("client/build"));
 }
 // Add routes, both API and view
-router.use(routes);
+app.get("/api/shelter", (req, res) => {
+  res.json(dbModel.selectAllShelter)
+})
+
 
 // Start the API server
 app.listen(PORT, function() {
