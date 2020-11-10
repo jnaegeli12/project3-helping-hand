873fd91ed4b047e57ef9fe25e69410409c954421
Judy Naegeli <jnaegeli@gmail.com>
2020-11-10 15:50:39 -0800

    updated connection for heroku deploy

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,17 @@
 const mysql = require("mysql");
 const util = require("util");
+let connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: process.env.JAWSDB_URL || 3306,
-  user: "root",
-  password: "employees",
-  database: "services_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'employees',
+    database: 'services_db'
+  })
+}
 
 connection.connect(function(err) {
   if (err) {
