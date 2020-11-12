04380fd2f2035970fe5641a87ab23b1bc7622751
Judy Naegeli <jnaegeli@gmail.com>
2020-11-11 19:57:39 -0800

    another tweak

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Results from './pages/Results';
 export default function App() {
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Nav />
         <Switch>
