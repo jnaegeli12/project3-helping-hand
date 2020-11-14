87fb5786c9748bcf824b9ec9316d20734a015105
Chris Parker <ctp7313@gmail.com>
2020-11-14 12:18:41 -0700

    created login page with route to registration

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,33 +1,12 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
-// import './App.css';
 import Axios from 'axios';
 
-// function Profile() {
-//     return(
-//         <div className="container container-fluid">
-//             <Header headerName={ "Create a Profile" }/>
-//         </div>
-//     )
-// }
-
-function Profile() {
-    const [registerUsername, setRegisterUsername] = useState('');
-    const [registerPassword, setRegisterPassword] = useState('');
+export default function Profile() {
     const [loginUsername, setLoginUsername] = useState('');
     const [loginPassword, setLoginPassword] = useState('');
-    const [data, setData] = useState(null);
-    const register = () => {
-      Axios({
-        method: 'POST',
-        data: {
-          username: registerUsername,
-          password: registerPassword,
-        },
-        withCredentials: true,
-        url: 'http://localhost:3000/register',
-      }).then((res) => console.log(res));
-    };
+    // const [data, setData] = useState(null);
+    
     const login = () => {
       Axios({
         method: 'POST',
@@ -39,50 +18,53 @@ function Profile() {
         url: 'http://localhost:3000/login',
       }).then((res) => console.log(res));
     };
-    const getUser = () => {
-      Axios({
-        method: 'GET',
-        withCredentials: true,
-        url: 'http://localhost:3000/user',
-      }).then((res) => {
-        setData(res.data);
-        console.log(res.data);
-      });
-    };
+
+    // const getUser = () => {
+    //   Axios({
+    //     method: 'GET',
+    //     withCredentials: true,
+    //     url: 'http://localhost:3000/user',
+    //   }).then((res) => {
+    //     setData(res.data);
+    //     console.log(res.data);
+    //   });
+    // };
+
     return (
-      <div className='App'>
-        <div>
-        <Header headerName={ "Create a Profile" }/>
-          <input
-            placeholder='username'
-            onChange={(e) => setRegisterUsername(e.target.value)}
-          />
-          <input
-            placeholder='password'
-            onChange={(e) => setRegisterPassword(e.target.value)}
-          />
-          <button onClick={register}>Submit</button>
+        <div className='container container-fluid'>
+            <Header headerName={ "Login" }/>
+            <div className="container container-fluid col-6 d-flex justify-content-center">
+                <form onSubmit={login}>
+                    <div className="form-group d-flex justify-content-around">    
+                        <label
+                            className="col-3"
+                            for="name">Username</label>
+                        <input
+                            className="col-5"
+                            placeholder='Username'
+                            onChange={(e) => setLoginUsername(e.target.value)}/>
+                    </div>
+                    <div className="form-group d-flex justify-content-around">    
+                        <label
+                            className="col-3"
+                            for="name">Password</label>
+                        <input
+                            className="col-5"
+                            placeholder='Password'
+                            onChange={(e) => setLoginPassword(e.target.value)}/>
+                    </div>
+                    <div className="form-group d-flex justify-content-around mb-3">    
+                        Don't have an account? <a href="/register">Sign up here</a>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <button type="button" onClick={login} className="btn btn-primary">Login</button>
+                    </div>
+                </form>
+       
+            </div>
         </div>
-  
-        <div>
-        <Header headerName={ "Login" }/>
-          <input
-            placeholder='username'
-            onChange={(e) => setLoginUsername(e.target.value)}
-          />
-          <input
-            placeholder='password'
-            onChange={(e) => setLoginPassword(e.target.value)}
-          />
-          <button onClick={login}>Submit</button>
-        </div>
-  
-        <div>
-        <Header headerName={ "Welcome Back!" }/>
-          <button onClick={getUser}>Submit</button>
-          {data ? <h1>Welcome Back {data.username}</h1> : null}
-        </div>
-      </div>
+
+         
+        
     );
-  }
-export default Profile;
+  };
